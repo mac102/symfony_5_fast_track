9d4fd530ac3ef495d3e253cf74d8fb2c5fd39d43
Maciek <mac102@wp.pl>
2020-12-03 16:40:47 +0100

    Dodano obsługę formularza encji comment.

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -10,14 +10,20 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Twig\Environment;
+use App\Entity\Comment;
+use App\Form\CommentFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class ConferenceController extends AbstractController
 {
     private $twig;
+    private $entityManager;
 
-    public function __construct(Environment $twig)
+    public function __construct(Environment $twig, EntityManagerInterface $entityManager)
     {
         $this->twig = $twig;
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -33,8 +39,29 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/conference/{slug}", name="conference")
      */
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir): Response
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
+            if ($photo = $form['photo']->getData()) {
+                $filename = bin2hex(random_bytes(6)) . '.' . $photo->guessExtension();
+                try {
+                    $photo->move($photoDir, $filename);
+                } catch (FileException $e) {
+                    // unable to upload the photo, give up
+                }
+                $comment->setPhotoFilename($filename);
+            }
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
@@ -43,19 +70,7 @@ class ConferenceController extends AbstractController
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
             'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comment_form' => $form->createView(),
         ]));
     }
-
-
-    // /**
-    //  * @Route("/conference/{slug}", name="conference")
-    //  */
-    // public function show(Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository): Response
-    // {
-    //     return new Response($this->twig->render('conference/show.html.twig', [
-    //         'conferences' => $conferenceRepository->findAll(),
-    //         'conference' => $conference,
-    //         'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']),
-    //     ]));
-    // }
 }
