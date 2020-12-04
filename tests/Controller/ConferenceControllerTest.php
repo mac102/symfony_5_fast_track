d19315206030bbebc7ab1d1671c69bd79027b17b
Maciek <mac102@wp.pl>
2020-12-04 12:10:43 +0100

    Dodano test przejscia na stronę konferencji.

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -14,4 +14,21 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback');
     }
+
+    public function testConferencePage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        // weryfikacja, czy są dwa nagłówki h4
+        $this->assertCount(2, $crawler->filter('h4'));
+
+        $client->clickLink('View');
+        // alternatywnie dla powyższego: $client->click($crawler->filter('h4 + p a')->link());
+
+        $this->assertPageTitleContains('Wawa');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Wawa 2018');
+        $this->assertSelectorExists('div:contains("There are 1 comments")');
+    }
 }
